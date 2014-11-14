d58184bfe88bca535fca0bb0d5dcb50a343de935
cdsteer <cdsteer94@gmail.com>
2014-11-14 15:18:29 +0000

    neater

--- readCSV.c
+++ readCSV.c
@@ -1,32 +1,33 @@
-
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-int main() {
-    
-    	char buffer[4096] ;
-    	char *record,*line;
+int main()
+{
+    char buffer[4096] ;
+    char *record,*line;
 	double item;
-    	int i=0,j=0,count=0;
-    	FILE *fstream = fopen("initial_data.csv","r");
+    int i=0,j=0;
+    // int count=0;
+    FILE *fstream = fopen("initial_data.csv","r");
     	
-	if(fstream == NULL) {
-        	printf("\n file opening failed ");
-        	return -1 ;
-    	}
+	if(fstream == NULL)
+    {
+        printf("\n file opening failed ");
+        return -1 ;
+    }
     
-   	while((line=fgets(buffer,sizeof(buffer),fstream))!= NULL) 
-	{
-        	record = strtok(line,",");
-		while(record = strtok(NULL,",") ){
+   	while((line=fgets(buffer,sizeof(buffer),fstream))!= NULL)
+    {
+        record = strtok(line,",");
+		while(record = strtok(NULL,",") )
+        {
 			printf("item: %8.3f \n",atof(record));
-			count++;
+			// count++;
 
 		}
-    	}
-   	printf("Entries counted, %d",count);
-    	return 0 ;
+    }
+   	// printf("Entries counted, %d",count);
+    return 0 ;
 }
 
 
